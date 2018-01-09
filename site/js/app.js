9d3a23c17e7b93d28edceb7e6d364ef0b8209b0c
shiv213 <shiv.v.trivedi@gmail.com>
2018-01-08 19:04:06 -0500

    login and signup finally completed

--- site/js/app.js
+++ site/js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(window.onload = function() {
     var config = {
         apiKey: "AIzaSyDTvUwyFOD-qb9trefu9jSL_ej-H4oqE5w",
         authDomain: "libraryapp-backend.firebaseapp.com",
@@ -15,23 +15,65 @@
     const btnSignup = document.getElementById('btnSignup')
     const btnLogout = document.getElementById('btnLogout')
 
-    btnLogin.addEventListener('click', e => {
-        console.log(5 + 6);
+    btnLogin.addEventListener('click', e=>{
+        console.log('login button click detected');
         const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
 
-        const promise = auth().signInWithEmailAndPassword(email, pass);
+        const promise = auth.signInWithEmailAndPassword(email, pass);
         promise.catch(e=>console.log(e.message));
 
     }
     );
-    btnSignup.addEventListener('click', e => {
-    	//TODO: Check for real email
-    	const email = txtEmail.value;
+    btnSignup.addEventListener('click', e=>{
+        console.log('signup button click detected');
+        //TODO: Check for real email
+
+        const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
 
-        const promise = auth().createUserWithEmailAndPassword(email, pass);
-        promise.catch(e=>console.log(e.message));
-    });
+        if (validateEmail(email)) {
+            const promise = auth.createUserWithEmailAndPassword(email, pass);
+        } else {
+            promise.catch(e=>console.log(e.message));
+        }
+
+    }
+    );
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email.toLowerCase());
+    }
+
+    function validate(email) {
+        if (validateEmail(email)) {
+            $("#result").text(email + " is valid :)");
+            $("#result").css("color", "green");
+        } else {
+            $("#result").text(email + " is not valid :(");
+            $("#result").css("color", "red");
+        }
+        return false;
+    }
+
+    btnLogout.addEventListener('click', e=>{
+        firebase.auth().signOut();
+    }
+    );
+
+    firebase.auth().onAuthStateChanged(firebaseUser=>{
+        if (firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+
+        } else {
+            console.log('Not logged in');
+            btnLogout.classList.add('hide');
+        }
+    }
+    );
+}
+);
